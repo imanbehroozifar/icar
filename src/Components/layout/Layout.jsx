7ad364eec17a852b20972065eca5d9c36fe4be37
Iman Behroozifar <105700046+imanbehroozifar@users.noreply.github.com>
2023-04-26 23:36:30 +0430

    Making the initial settings, making the header and footer of the project, this project is to show the skill of routing in next

---
+++ src/Components/layout/Layout.jsx
@@ -0,0 +1,21 @@
+import styles from './Layout.module.css'
+import Link from 'next/link'
+const Layout = ({children}) =>
+{
+  return (
+    <>
+      <header className={styles.header}>
+        <Link href='/'>
+        <h2>ICAR</h2> 
+        <p>Choose and bye your car</p>
+        </Link>
+      </header>
+      <div className={styles.container}>{ children }</div>
+      <footer className={styles.footer}>
+        ICar project &copy;
+      </footer>
+    </>
+  )
+}
+
+export default Layout
